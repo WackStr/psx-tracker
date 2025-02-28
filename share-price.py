79991d381a8f6a463be5a157cb13d52d9c21558e
WackStr <waqasrazajafri@gmail.com>
2025-03-01 02:47:56 +0500

    Setup database (postgres)

--- share-price.py
+++ share-price.py
@@ -1,6 +1,13 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 import time
+
+db_host = os.environ.get("DB_HOST")
+db_port = os.environ.get("DB_PORT")
+db_name = os.environ.get("DB_NAME")
+db_user = os.environ.get("DB_USER")
+db_password = os.environ.get("DB_PASSWORD")
 
 def get_price(script: str) -> float:
     try:
@@ -29,3 +36,4 @@ def get_price_for_scripts(scripts: set[str]) -> dict[str, float]:
 if __name__=="__main__":
     scripts = {}
     print(get_price_for_scripts(scripts))
+    
