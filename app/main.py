89d4ab7e34c120ffa6582daae059e9f2d38962af
WackStr <waqasrazajafri@gmail.com>
2025-03-05 20:11:43 +0500

    Integrated pydantic and defined models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
 
-@app.get("/health")
-async def health_check():
-    """Health end point"""
-    return {"status": "ok"}
+class Company(BaseModel):
+    category: str
+    company_name: str
+    script: str
+    
+class CompanyMeasurement(BaseModel):
+    market_cap: float
+    shares: int
+    free_float: int
+    free_float_percentage: float
+    
+class IndexSegment(BaseModel):
+    company: Company
+    percentage: float
+    
+class SharePrice(BaseModel):
+    company: Company
+    price: float
+    timestamp: datetime
+    
+
+@app.get("/share-price/{script}")
+async def get_share_price(script: str):
+    """get_price_of_share"""
+    return {"share-price": 10.0}
