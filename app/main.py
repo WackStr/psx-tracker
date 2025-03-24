f261654f9e467bc45cae9035a1628f394c44afed
WackStr <waqasrazajafri@gmail.com>
2025-03-25 02:32:47 +0500

    allowed cors for local development

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from .database import get_db
 from .models.share_price import share_price
 
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/share-price/{script}")
 async def get_share_price(script: str, db: Session  = Depends(get_db)):
